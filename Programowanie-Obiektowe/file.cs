bb312e7ec34433f226edfdcd88b681b3d308418c
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-10-16 11:59:29 +0200

    first

--- Programowanie-Obiektowe/file.cs
+++ Programowanie-Obiektowe/file.cs
@@ -1,11 +1,9 @@
 using System.Data;
-using Internal;
 using System.Reflection.PortableExecutable;
 using System.IO.Enumeration;
 using System.IO;
 using System.Security.AccessControl;
 using System;
-using System.Collection.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;

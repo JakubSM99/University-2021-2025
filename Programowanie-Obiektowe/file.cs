57560f39a8a3dbdb8fdb495fb599af4287024f77
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-10-16 11:20:23 +0200

    Create file.cs

---
+++ Programowanie-Obiektowe/file.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using Internal;
+using System.Reflection.PortableExecutable;
+using System.IO.Enumeration;
+using System.IO;
+using System.Security.AccessControl;
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przywitanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj, świecie!");
+            Console.ReadKey();
+        }
+    }
+}

f31d8f059b17720e40d7cd3430b87792fe768010
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-11-06 10:54:38 +0100

    idk

---
+++ Programowanie-Obiektowe/Zadania/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 6;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
